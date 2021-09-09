4aa66b137a8929d9fd52c3188e09dafcfd5e7172
miumo <miumo2020@gmail.com>
2021-09-09 23:50:53 +0900

    Update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import { Helmet } from 'react-helmet';
 
 import { css } from '@emotion/react';
@@ -56,43 +57,31 @@ const About: React.FC = () => (
 
             <PostFullContent className="post-full-content">
               <div className="post-content">
-                <h5>
-                  A starter template for Gatsby <br /> GitHub: <a href="https://github.com/scttcper/gatsby-casper">scttcper/gatsby-casper</a>
-                </h5>
+                <h3>当サイトについて</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo finibus leo,
-                  non tempus magna vehicula ac. Maecenas mollis ante finibus pharetra imperdiet.
-                  Maecenas in aliquam purus. Nam et massa a nulla fermentum dapibus sit amet in
-                  neque. Ut ipsum ipsum, rhoncus a sodales pellentesque, interdum a elit. Nullam
-                  aliquam tellus nibh, eget laoreet dui aliquet non. Vestibulum malesuada ante at
-                  diam tempus, ac interdum risus scelerisque. Sed ipsum neque, vulputate porta diam
-                  eget, consequat blandit nulla. Integer volutpat velit vitae purus lacinia aliquam.
-                  Integer bibendum ipsum vitae magna pulvinar, nec vehicula dolor vulputate. Nulla
-                  eu massa id orci placerat finibus vel sit amet eros. Vestibulum quis consequat
-                  massa. Sed sagittis sollicitudin massa at commodo. Praesent diam nisi, imperdiet
-                  posuere eleifend nec, blandit ac massa.
+                  日々の作業記録、コードの覚書等を掲載します。<br></br>
+                  基本的には自分用ですが、どこかで誰かの役に立てば幸いです。<br></br>
+                  GitHub：
+                  <Link to={`https://github.com/miumo2020`}>https://github.com/miumo2020</Link>
                 </p>
+
+                <h3>免責事項</h3>
                 <p>
-                  Vestibulum semper pretium ipsum nec congue. Ut ac eros nisi. Donec leo sem,
-                  aliquam mollis sapien ultrices, dapibus congue diam. Proin viverra dapibus
-                  blandit. Ut mauris tellus, tristique id felis vel, venenatis vestibulum nunc. Nam
-                  molestie pulvinar nibh, eget egestas augue. Maecenas tellus arcu, mattis ut ipsum
-                  non, sollicitudin convallis nunc. Donec nec neque tristique, aliquet lacus id,
-                  laoreet nunc. Cras dapibus nisi nulla, ullamcorper faucibus neque suscipit ac.
-                  Donec eget orci venenatis justo lobortis volutpat. Proin vel placerat nisl.
-                  Integer arcu nunc, sodales eu fringilla non, aliquam non diam. Cras placerat,
-                  massa et faucibus pretium, ante elit tincidunt tellus, tristique ultricies velit
-                  quam et massa.
+                  当サイトのコンテンツ・情報につきまして、可能な限り正確な情報を掲載するよう努めておりますが、
+                  必ずしもそれらの正確性や安全性を保障するものではありません。
+                  当サイトをご利用することで発生したトラブルや損害に関しては一切の責任を負いかねますので、あらかじめご了承ください。
+                  <br></br>
+                  本免責事項、および当サイトに掲載しているすべての記事は、予告なく変更・削除されることがあります。
                 </p>
+                <h3>著作権・肖像権について</h3>
                 <p>
-                  In nunc lacus, dapibus vitae lacus sit amet, efficitur iaculis neque. Suspendisse
-                  ut tellus quis leo vestibulum tincidunt. Aenean nec enim ac dolor lacinia semper.
-                  Ut sed laoreet libero. Nunc elementum sollicitudin accumsan. Nunc eu augue neque.
-                  Proin a tortor nibh. Cras eu nisl ornare sapien feugiat pellentesque. Mauris
-                  dignissim vel quam eu pellentesque. Integer sit amet posuere quam, eu ullamcorper
-                  odio. Nullam a lacus tempus sapien dignissim ullamcorper. In hac habitasse platea
-                  dictumst. Proin quis massa aliquam, feugiat tortor sit amet, tincidunt urna. Donec
-                  posuere pulvinar lectus, ac semper ipsum vulputate quis.
+                  当サイトに掲載されている文章・画像・動画等の著作物の情報を無断で転載・使用することを禁止します。
+                  引用する際は、引用元を明示いただくようお願い致します。
+                </p>
+
+                <h3>リンクについて</h3>
+                <p>
+                  当サイトはリンクフリーですが、画像への直リンクやインラインフレームの使用はお断り致します。
                 </p>
               </div>
             </PostFullContent>
